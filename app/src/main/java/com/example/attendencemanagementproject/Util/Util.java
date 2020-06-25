3035a7603721ef340790efbf1ff3686a8d1e895a
warlockcode <51631418+warlockcode@users.noreply.github.com>
2020-06-25 12:22:36 +0530

    Update Util.java

---
+++ app/src/main/java/com/example/attendencemanagementproject/Util/Util.java
@@ -0,0 +1,21 @@
+package com.example.attendencemanagementproject;
+public class Util {
+    //Database related items
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Student_db";
+    public static final String TABLE_NAME = "Student";
+    //contacts table columns names
+    public static final String KEY_ID="id";
+    public static final String KEY_STUDENT_ID = "stu_id";
+    public static final String KEY_STUDENT_NAME = "stu_name";
+    public static final String KEY_PHONE_NUMBER = "phone_number";
+    public static final String KEY_FATHER_NAME="father_name";
+    public static final String KEY_STUDENT_COURSE="course";
+    public static final String KEY_SEMESTER="semester";
+    public static final String KEY_YEAR ="year";
+    public static final String KEY_GENDER="gender";
+    public static final String KEY_ROLL_NUMBER="roll_number";
+    public static final String KEY_BRANCH="branch";
+    public static final String KEY_DOB="DOB";
+    public static final String KEY_PASSWORD ="password";
+}
