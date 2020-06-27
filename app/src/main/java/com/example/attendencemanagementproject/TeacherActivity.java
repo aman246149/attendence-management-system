4dda37befe8807be4d9284911c4a0eda8c26d88d
aman singh <amanthapliyal14@gmail.com>
2020-06-27 06:36:40 +0530

    ui and all bug fixed teacher authorization is being made ,attence view of student is being fixed

--- app/src/main/java/com/example/attendencemanagementproject/TeacherActivity.java
+++ app/src/main/java/com/example/attendencemanagementproject/TeacherActivity.java
@@ -6,9 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.attendencemanagementproject.data.DATABASE_HANDLER_Student;
+
+import org.w3c.dom.Text;
 
 public class TeacherActivity extends AppCompatActivity {
     private Button LoginButton;
+    private EditText TeacherId;
+    private EditText TeacherLogin;
+    boolean passwardchecker;
+    boolean id_checker;
+
+    DATABASE_HANDLER_Student db=new DATABASE_HANDLER_Student(TeacherActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +28,38 @@ public class TeacherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_teacher);
 
         LoginButton=findViewById(R.id.loginbuttonstudentid);
+        TeacherId=findViewById(R.id.editTextTextPersonName);
+        TeacherLogin=findViewById(R.id.editTextTextPersonName2);
 
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(TeacherActivity.this,TeacherAttendenceRegister.class);
-                startActivity(intent);
+
+                String id=TeacherId.getText().toString();
+                String pass=TeacherLogin.getText().toString();
+
+                id_checker=db.if_avilable(id);
+                if(id_checker)
+                {
+                    passwardchecker=db.check_Password(id,pass);
+
+                    if(passwardchecker)
+                    {
+                        Intent intent=new Intent(TeacherActivity.this,TeacherAttendenceRegister.class);
+                        startActivity(intent);
+
+                    }
+                    else
+                        Toast.makeText(TeacherActivity.this, "wrong password", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(TeacherActivity.this, "wrong student id", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
             }
         });
     }
